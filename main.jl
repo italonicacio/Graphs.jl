4a564a244076cb1b9f4bbd9fbecb99753b0e82a4
italonicacio <italonicacio@gmail.com>
2021-09-26 03:06:16 -0300

    First TGA activity

--- main.jl
+++ main.jl
@@ -5,5 +5,186 @@ file_name = "instances/graph1.txt"
 
 G = ReadInstance(file_name)
 
-mean_degree(G)
 
+# I) Exibir os graus mínimo, máximo e medio
+println("I)")
+println("Grau minimo ", δ(G))
+println("Grau maximo ", Δ(G))
+println("Grau medio ", mean_degree(G))
+println()
+
+
+
+# II) Exibir a sequencia de graus de G
+println("II)")
+println("Sequencia de Graus de G:")
+display(vertices_degree(G))
+println()
+
+
+
+# III) O grau e as vizinhancas aberta e fechada de um vertice v 
+# informado pelo usuario
+println("III)")
+println("Grau, vizinhancas aberta e fechada do vertice v")
+v = 1
+println("Vertice v = ", v)
+println("Grau de v")
+println(d(G, v))
+println()
+println("Vizinhanca fechada:")
+display(closed_neighbourhood(G, v))
+println()
+println("Vizinhanca aberta:")
+display(open_neighbourhood(G, v))
+println()
+
+
+
+
+# IV) Determinar se dois vertices u e v 
+# informados pelo usuario sao vizinhos
+println("IV)")
+println("Verificar se u e v são vizinhos:")
+u = 2
+v = 3
+println("u = ", u)
+println("v = ", v)
+is_neighbor = isneighbor(G, v, u)
+
+if is_neighbor
+    println("u e v são vizinhos")
+else 
+    println("u e v não são vizinhos")
+end
+
+println()
+
+
+
+
+# V) Informar se o grafo e k-regular, deixando claro o valor de k
+println("V)")
+k = isregular(G)
+
+if k == 0
+    println("O Grafo G não é regular")
+else
+    println("O Grafo G é ", k, " regular")
+end
+
+println()
+
+
+
+# VI) Informar se G e completo
+println("VI)")
+is_complete = iscomplete(G)
+
+if is_complete
+    println("O Grafo G não é completo")
+else
+    println("O Grafo G é completo")
+end
+
+println()
+
+
+
+
+# VII) Listar todos os vertices universais de G
+println("VII)")
+universal = universal_vertices(G)
+display(universal)
+
+println()
+
+
+
+
+# VIII) Listar todos os vertices isolados de G
+println("VIII)")
+isolated = isolated_vertices(G)
+display(isolated)
+
+println()
+
+
+
+# IX) Determinar o subgrafo de G induzido por um conjunto de vertices 
+# S ⊆ V (G) informado pelo usuario. Tal subgrafo e denotado por G[S
+println("IX)")
+S = [1, 2, 3]
+println("S:")
+display(S)
+println()
+
+Gv_induced = vertex_induced(G, S)
+println("G[S]: ")
+println("V(G[S]):")
+display(Gv_induced[1])
+println()
+println("E(G[S]):")
+display(Gv_induced[2])
+
+println()
+
+
+# X) Determinar o subgrafo de G induzido por um conjunto de arestas E0 ⊆ E(G) 
+# informado pelo usuario. Tal subgrafo e denotado por G[E']
+println("X)")
+E_ = [(1,2), (2,3), (5, 6)]
+println("E':")
+display(E_)
+println()
+
+Ge_induced = edge_induced(G, E_)
+println("G[E']: ")
+println("V(G[S]):")
+display(Ge_induced[1])
+println()
+println("E(G[S]):")
+display(Ge_induced[2])
+
+println()
+
+
+
+# XI) Decidir se uma sequencia de vertices v1, v2, ..., vk informada pelo usuario representa um passeio
+# em G. Em casa afirmativo, informar se o passeio tambem constitui um ciclo ou um caminho;
+println("XI)")
+seq = [1, 2, 4, 3]
+
+walk_path_cycle = is_walk_path_cycle(G, seq)
+if walk_path_cycle[1]
+    println("Essa sequencia é um passeio.")
+    if walk_path_cycle[3]
+        println("Essa sequencia é um ciclo.")
+    elseif walk_path_cycle[2]
+        println("Essa sequencia é um caminho.")
+    end
+end
+
+println()
+
+
+
+
+# XII) Determinar se um conjunto de vertices S ⊆ V (G) 
+# informado pelo usuario e uma clique de G. Em caso afirmativo, 
+# informar se a clique e maximal.
+println("XII)")
+println("Conjunto S:")
+S_ = [1, 2, 3, 4]
+display(S_)
+
+is_clique = isclique(G, S_)
+if is_clique > 0
+    if is_clique == 1
+        println("S é uma clique.")
+    else
+        println("S é uma clique maximal.")
+    end
+else
+    println("Não é uma clique.")
+end
--- src/graph.jl
+++ src/graph.jl
@@ -2,7 +2,23 @@
 export  Graph, 
         δ, delta, 
         Δ, Delta,
-        mean_degree
+        mean_degree,
+        d,
+        vertices_degree_unordered,
+        vertices_degree,
+        closed_neighbourhood,
+        open_neighbourhood,
+        isneighbor,
+        isregular,
+        iscomplete,
+        universal_vertices,
+        isolated_vertices,
+        vertex_induced,
+        subgraph_vertex_induced,
+        edge_induced,
+        subgraph_edge_induced,
+        is_walk_path_cycle,
+        isclique
 
 struct Graph
     n
@@ -36,17 +52,17 @@ end
 """
 Grau minimo de um Grafo
 """
-δ(G) = delta(G)
+δ(G::Graph) = delta(G)
 
 """
 Grau maximo de um Grafo
 """
-Δ(G) = Delta(G)
+Δ(G::Graph) = Delta(G)
 
 """
 Grau minimo de um Grafo
 """
-function delta(G)
+function delta(G::Graph)
     min = Inf
     vertex = 0
     for i = 1:G.n
@@ -65,7 +81,7 @@ end
 """
 Grau maximo de um Grafo
 """
-function Delta(G)
+function Delta(G::Graph)
     max = -1
     vertex = 0
     for i = 1:G.n
@@ -80,6 +96,364 @@ function Delta(G)
     return max
 end
 
-function mean_degree(G) 
-    return Int64(round(2*G.m/G.n))
+"""
+Retorna o grau medio do Grafo
+"""
+function mean_degree(G::Graph) 
+    return 2*G.m/G.n
+end
+
+
+"""
+Retorna o grau do vertice v
+"""
+function d(G, v) 
+    return length(G.adjacency_list[v])
+end
+
+"""
+Retorna o Grau dos Vertices do Grafo não ordenado
+"""
+function vertices_degree_unordered(G)
+    unordered_degrees = []
+    for i = 1:G.n
+        push!(unordered_degrees, length(G.adjacency_list[i]))
+    end
+
+    return unordered_degrees
+end
+
+"""
+Retorna o Grau dos Vertices do Grafo
+"""
+function vertices_degree(G)
+    vertices_degree = vertices_degree_unordered(G)
+    sort!(vertices_degree)
+    return vertices_degree
+end
+
+"""
+Retorna a vizinhança fechada do vertice v, a primeira posição do array com a vizinhança é o vertice v
+"""
+function closed_neighbourhood(G, v)
+    closed_neigh = G.adjacency_list[v]
+    return append!([v], closed_neigh)
+end
+
+"""
+Retorna a vizinhança fechada do vertice v
+"""
+function open_neighbourhood(G, v)
+    return G.adjacency_list[v]
+end
+
+
+"""
+Retorna true ou false se os vertices u e v forem vizinhos ou não respectivamente
+"""
+function isneighbor(G, v, u)
+    if G.adjacency_matrix[v, u] == 1
+        return true
+    else
+        return false
+    end
+end
+
+
+"""
+Verifica se um Grafo G é k-regular e retorna 0 se G não for regular e se for regular retorna o valor de k
+"""
+function isregular(G)
+    vertex_degrees = vertices_degree(G)
+    
+    if all(i -> i == vertex_degrees[1], vertex_degrees)
+        return vertex_degrees[1]
+    else
+        return 0
+    end
+
+end
+
+"""
+Verifica se um Grafo G é completo e retorna true se G for completo e false se não
+"""
+function iscomplete(G)
+    if isregular(G) > 0
+        if G.m == G.n*(G.n-1)/2
+            return true
+        else
+            return false
+        end
+    else
+        return false 
+    end
+    
+end
+
+"""
+Retorna os vertices universais do Grafo G, se não houver vertices universais retorna um array vazio
+"""
+function universal_vertices(G)
+    vertex_degrees = vertices_degree_unordered(G)
+    universal_vertices = []
+
+    for i = 1:G.n
+        if vertex_degrees[i] == G.n - 1
+            push!(universal_vertices, i)
+        end
+    end
+    
+
+    return universal_vertices
+end
+
+"""
+Retorna os vertices isolados do Grafo G, se não houver vertices isolados retorna um array vazio
+"""
+function isolated_vertices(G)
+    vertex_degrees = vertices_degree_unordered(G)
+    isolated_vertices = []
+    
+    for i = 1:G.n
+        if vertex_degrees[i] == 0
+            push!(isolated_vertices, i)
+        end
+    end
+    
+    return isolated_vertices
+end
+
+"""
+Retorna um conjunto de vertices e arestas que representa um subgrafo de G
+"""
+function vertex_induced(G, V)
+    E = []
+    n = length(V)
+
+    for i = 1:n
+        v = V[i]
+        for j = i+1:n
+            u = V[j]
+            if G.adjacency_matrix[v, u] != 0
+                push!(E, (v, u))
+            end
+        end
+    end
+
+    return (V, E)
+end
+
+"""
+Retorna um subgrafo induzido por um subconjunto de vertices do Grafo G
+"""
+function subgraph_vertex_induced(G, V)
+    E = []
+    n = length(V)
+    adj_matrix = zeros(Int64, n, n)
+
+    for i = 1:n 
+        v = V[i]
+        for j = i+1:n
+            u = V[j]
+            if G.adjacency_matrix[v, u] != 0
+                push!(E, (v, u))
+            end
+        end
+    end
+
+
+    for i = 1:n
+        v = V[i]
+        
+        for j = i+1:n
+            u = V[j]
+            if G.adjacency_matrix[v, u] != 0
+                adj_matrix[i, j] = G.adjacency_matrix[v, u]
+                adj_matrix[j, i] = G.adjacency_matrix[u, v]
+            end
+
+            # Necessario apenas quando utilizar multigrafos
+            # if G.adjacency_matrix[u, v] != 0
+            #     adj_matrix[j, i] = G.adjacency_matrix[u, v]
+            # end
+
+        end
+    end
+
+    return creat_graph(n, adj_matrix)
+end
+
+"""
+Retorna um conjunto de vertices e arestas que representa um subgrafo de G
+"""
+function edge_induced(G, E)
+
+    V = []
+    for i = 1:length(E)
+        e = E[i]
+
+        if !(e[1] in V)
+            push!(V, e[1])
+        end
+
+        if !(e[2] in V)
+            push!(V, e[2])
+        end
+
+    end
+
+    return (V, E)
+end
+
+"""
+Retorna um subgrafo induzido por um subconjunto de arestas do Grafo G
+"""
+function subgraph_edge_induced(G, E)
+
+    V = []
+    for i = 1:length(E)
+        e = E[i]
+
+        if !(e[1] in V)
+            push!(V, e[1])
+        end
+
+        if !(e[2] in V)
+            push!(V, e[2])
+        end
+
+    end
+
+    n = length(V)
+    adj_matrix = zeros(Int64, n, n)
+    
+    for i = 1:n
+        for j = i+1:n
+
+        end
+    end
+    
+    for k = 1:length(E)
+        u = E[k][1]
+        v = E[k][2]
+
+        i = -1
+        j = -1
+        for w = 1:n
+            
+            if u == V[w]
+                i = w
+            end
+
+            if v == V[w]
+                j = w
+            end
+
+            if i != -1 && j != -1
+                break
+            end
+        end
+
+        adj_matrix[i, j] = G.adjacency_matrix[u, v]
+        adj_matrix[j, i] = G.adjacency_matrix[v, u]
+    end
+
+
+    return creat_graph(n, adj_matrix)
+end
+
+"""
+Retorna um array de boleanos com 3 posições onde cada uma diz respeito 
+se a sequencia é um passeio, um caminho ou um ciclo respectivamente
+"""
+function is_walk_path_cycle(G, sequence)
+    walk_path_cycle = [true, true, false]
+    
+    n = length(sequence)
+    is_visited = Dict(sequence[i] => false for i = 1:n)
+
+    current_vertex = sequence[1]
+    is_visited[current_vertex] = true
+    for i = 2:n-1
+        aux_vertex = sequence[i]
+
+        if G.adjacency_matrix[current_vertex, aux_vertex] != 1
+            walk_path_cycle[1] = false
+            walk_path_cycle[2] = false
+            return walk_path_cycle
+        end
+        
+        if is_visited[aux_vertex] == false
+            is_visited[aux_vertex] = true
+        else
+            walk_path_cycle[2] = false
+        end
+
+        current_vertex = aux_vertex
+        
+    end
+
+    if G.adjacency_matrix[current_vertex, sequence[n]] == 0
+        walk_path_cycle[1] = false
+        walk_path_cycle[2] = false
+        walk_path_cycle[3] = false
+        return walk_path_cycle
+        
+    elseif walk_path_cycle[2]
+        
+        if sequence[1] == sequence[n]
+            walk_path_cycle[3] = true
+            return walk_path_cycle
+        elseif is_visited[sequence[n]]
+            walk_path_cycle[2] = false
+            walk_path_cycle[3] = false
+        end
+    end
+
+    return walk_path_cycle
+end
+
+
+"""
+Retorna 0 se o conjunto de vertices v não é uma clique, retorna 1 se for uma clique
+e retorna 2 se for uma clique maximal
+"""
+function isclique(G, S)
+    
+    H = subgraph_vertex_induced(G, S)
+    d_G = vertices_degree_unordered(G)
+    is_clique = 0
+    if iscomplete(H)
+        is_clique = 1
+        
+        is_visited = [false for i = 1:G.n]
+        for i in S
+            is_visited[i] = true
+        end
+        
+        aux = S
+        is_maximal = true
+        for i = 1:G.n
+        
+            if !is_visited[i]
+                push!(aux, i)
+
+                aux_G = subgraph_vertex_induced(G, aux)
+
+                if iscomplete(aux_G)
+                    is_maximal = false
+                end
+
+                aux = S
+            end
+            
+        end
+
+        if is_maximal
+            is_clique = 2
+        end
+    end
+
+
+    return is_clique
 end
